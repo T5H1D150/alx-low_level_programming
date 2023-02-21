16753e2b8f779f0384423f816059a1cd7ae1c8b9
Tshidiso <bhut_mlenzi@outlook.com>
2023-02-20 17:45:45 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,10 @@
 /**
  * main - c Program that prints a line to standard error 
  *
- * Return: Always 1 (Success)
+ * Return 1 (Success)
  */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);		
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
