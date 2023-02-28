6953fb7b5871ffec5f8c5a8a1ca4230258aed35b
Tshidiso <bhut_mlenzi@outlook.com>
2023-02-28 09:27:03 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,8 +11,9 @@
 int print_sign(int n)
 {
 	if (n > 0)
-	{	_putchar('+');
-		return (1):
+	{
+		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
@@ -21,7 +22,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		__putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 }
