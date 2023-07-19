53789fec6b78bb21a407d980b67accdee438ff89
Tshidiso <bhut_mlenzi@outlook.com>
2023-07-19 06:23:28 -0700

    print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 Always
+ *
+ **/
+
+int main(void)
+{
+	char Alpha = 'a';
+
+	while (Alpha <= 'z')
+	{
+		putchar(Alpha);
+		Alpha++;
+	}
+putchar('\n');
+return (0);
+}
