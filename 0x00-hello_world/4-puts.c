4bc715655e119cd1e0b443590ec957b2ca3d0653
Tshidiso <bhut_mlenzi@Outlook.com>
2023-09-07 08:06:28 -0700

    Yaay

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 Always
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
