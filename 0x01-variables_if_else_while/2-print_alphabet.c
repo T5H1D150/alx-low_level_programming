11834de72b3930df14899134869d07fc0ffd330a
Tshidiso <bhut_mlenzi@Outlook.com>
2023-09-18 11:55:20 -0700

    again

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * main - Print letters of the alphabet
+ *
+ * Return: 0 Always
+ *
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
