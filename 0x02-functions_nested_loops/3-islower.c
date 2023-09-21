ce7c569df6666a0a8f00904dc322164e61a9c2ba
Tshidiso <bhut_mlenzi@Outlook.com>
2023-09-21 06:22:55 -0700

    IntC

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - Is character lower or upper case
+ * @c: accepts character
+ * Return: 1 if c is lower case, else returns 0
+ *
+ */
+
+int _islower(int c)
+{
+	char i;
+
+	c = 1;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+	return (c);
+	}
+	return (c);
+}
