293e98124166a234293c2cdd96d197af1400c641
Tshidiso <bhut_mlenzi@Outlook.com>
2023-09-22 01:19:02 -0700

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Function for abs value of integer
+ * @x: accepts integer value
+ * Return: Abs value
+ *
+ */
+
+int _abs(int x)
+{
+	int y = -1;
+
+	if (x >= 0)
+		return (x);
+	else
+		return (x * y);
+}
