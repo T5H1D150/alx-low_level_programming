0bf365dfd29236ef9b76f732a1ee31aadd17d4ed
Tshidiso <tshidiso_moletsane@yahoo.com>
2023-12-14 11:35:13 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,17 +3,6 @@
 /**
  * main - Entry point
  *
-<<<<<<< HEAD
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-=======
  * Return: 0 Always
  *
  */
@@ -25,6 +14,5 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
->>>>>>> 4adf91016d35cb2fe697a35aadea74930d3f0e6e
 	return (0);
 }
