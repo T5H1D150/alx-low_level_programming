3b163218b8b8bc56a1c926b782322c5f78d9ea6e
Tshidiso <tshidiso_moletsane@yahoo.com>
2023-12-14 11:23:08 -0800

    grammar

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
  * main - Entry point
  *
  * Return: Always 0 (Success)
-=======
  * main - Entry Point
  *
  * Return: 0 Always
->>>>>>> 4adf91016d35cb2fe697a35aadea74930d3f0e6e
  */
 
 int main(void)
