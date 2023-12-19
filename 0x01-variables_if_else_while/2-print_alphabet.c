59e47eadf1bacc82ddfc4a876e0ae2ed04513546
Tshidiso <tshidiso_moletsane@yahoo.com>
2023-12-19 08:18:03 -0800

    Alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Always
+ *
+ */
+
+int main(void)
+{
+	char Alpha;
+
+	for (Alpha = 'a' ; Alpha <= 'z' ; Alpha++)
+	{
+	putchar(Alpha);
+	}
+	putchar('\n');
+	return (0);
+}
