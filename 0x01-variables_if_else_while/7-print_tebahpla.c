0adbd6e453e9a4ceea0b43df4f5962d150db82e7
Tshidiso <tshidiso_moletsane@yahoo.com>
2023-12-19 09:17:03 -0800

    BetAlpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 always
+ *
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'z' ; x >= 'a' ; x--)
+	{
+	putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
