0fbbd1e125033f9a6693b6d23b0c3afa1f47f52a
Tshidiso <tshidiso_moletsane@yahoo.com>
2023-12-19 08:57:38 -0800

    numnum

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 always
+ *
+ *
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+	printf("%d", num);
+	num++;
+	}
+	putchar('\n');
+	return (0);
+}
