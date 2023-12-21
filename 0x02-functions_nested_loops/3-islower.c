7b1d8571f9fdad87f90cbc4960d8826ae4780dc7
Tshidiso <tshidiso_moletsane@yahoo.com>
2023-12-21 08:08:55 -0800

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ * @c: Alphabet
+ * Return: 1 if c is lowercase, else 0
+ *
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'a')
+		return (1);
+	else
+		return (0);
+}
