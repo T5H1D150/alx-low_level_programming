2ab99fce5521349cd5717250b54cac2fe6a90c96
Tshidiso <tshidiso_moletsane@yahoo.com>
2023-12-21 07:18:39 -0800

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print Alpha 10x
+ * Return: None
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int num;
+
+	for (num = 1 ; num <= 10 ; num++)
+	{
+		for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+		{
+		_putchar(alpha);
+		}
+	_putchar('\n');
+	}
+
+	
+}
