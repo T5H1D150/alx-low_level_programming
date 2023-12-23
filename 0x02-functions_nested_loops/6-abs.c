31891c8b84fd6fa529ef83926d35d51b7f07e29f
Tshidiso <tshidiso_moletsane@yahoo.com>
2023-12-23 07:27:46 -0800

    Return

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - gives absolute value
+ * @n: accepts value
+ * Return: 0 always
+ */
+
+int _abs(int n)
+{
+	int y = -1;
+
+	if (n >= 0)
+		return (n);
+	else
+		return ( n * y);
+}
