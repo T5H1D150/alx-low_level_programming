7571636a6667e6e865b09707d145ed7cb4f78f23
Tshidiso <tshidiso_moletsane@yahoo.com>
2024-01-03 10:01:33 -0800

    24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - returns every minute over 24 hrs
+ *
+ * Return: 0 always
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hrs, min;
+
+	for (hrs = 0 ; hrs <= 23 ; hrs++)
+	{
+		for (min = 0 ; min <= 59 ; min++)
+		{
+		_putchar((hrs / 10) + '0');
+		_putchar((hrs % 10) + '0');
+		_putchar(58);
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar('\n');
+		}
+	}
+}
