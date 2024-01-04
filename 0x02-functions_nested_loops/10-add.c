bbeb66c7763e9ae3183d5c871e13e2f183f7ebd6
Tshidiso <tshidiso_moletsane@yahoo.com>
2024-01-04 04:44:34 -0800

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds to numbers
+ * @a: accepts 1st value
+ * @b: accepts 2nd value
+ * Return: Result
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
