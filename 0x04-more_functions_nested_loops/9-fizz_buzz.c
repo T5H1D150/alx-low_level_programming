f2b1a82bf960fe0adb8b021395dda4fc47bfabb2
Tshidiso <bhut_mlenzi@Outlook.com>
2024-01-10 15:47:06 +0000

    Fizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - FizzBuzz
+ * Return: O Always
+ *
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1 ; num <= 100 ; num++)
+	{
+		if (num % 15 == 0)
+			printf("FizzBuzz");
+		else if (num % 5 == 0)
+			printf("Buzz");
+		else if (num % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%d", num);
+		printf(" ");
+	}
+	return (0);
+}
