e66aa3098fd470758c9e9a0014af7ed606d92ab6
Tshidiso <tshidiso_moletsane@yahoo.com>
2024-01-09 20:14:56 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies 2 ints
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: Result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
